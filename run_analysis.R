3fa4aae25755a1b821a990e9801f6aa421f2ce6a
perttvil <vili.perttila@gmail.com>
2014-11-19 17:55:10 +0200

    Script is ready

--- run_analysis.R
+++ run_analysis.R
@@ -14,45 +14,39 @@ if(!file.exists(extractedDir)) {
 
 # read feature names
 featureTable <- read.table("extracted/UCI HAR Dataset/features.txt", col.names=c("FEATURE_ID", "FEATURE"), row.names=NULL)
-featureNames <- featureTable$FEATURE
+featureNames <- make.unique(as.character(featureTable$FEATURE))
 
 # read activities
-activityTable <- as.data.table(read.table("extracted/UCI HAR Dataset/activity_labels.txt", col.names=c("ACTIVITY_ID", "ACTIVITY"), row.names=NULL, colClasses = c("numeric", "character")))
+activityTable <- read.table("extracted/UCI HAR Dataset/activity_labels.txt", col.names=c("ACTIVITY_ID", "ACTIVITY"), row.names=NULL, colClasses = c("numeric", "character"))
 
 # read train data
 trainSubject <- as.data.table(read.table("extracted/UCI HAR Dataset/train/subject_train.txt", col.names=c("SUBJECT"), row.names=NULL, colClasses = c("numeric")))
 trainActivity <- as.data.table(read.table("extracted/UCI HAR Dataset/train/y_train.txt", col.names=c("ACTIVITY_ID"), row.names=NULL, colClasses = c("numeric")))
-trainData <- as.data.table(read.table("extracted/UCI HAR Dataset/train/x_train.txt", col.names=featureNames, row.names=NULL))
+trainData <- as.data.table(read.table("extracted/UCI HAR Dataset/train/x_train.txt", col.names=featureNames, row.names=NULL, check.names = FALSE))
 
 # Add subject to train data
-trainData$SUBJECT <- trainSubject[,1]
+trainData$SUBJECT <- trainSubject$SUBJECT
 # add activities to train data
 trainActivitiesTable <- trainActivity[,ACTIVITY := activityTable[ACTIVITY_ID,2]]
-
-print(trainActivity[1:30,])
-print(trainActivitiesTable[1:30,])
+trainData$ACTIVITY <- trainActivitiesTable$ACTIVITY
 
 #read test data
 testSubject <- as.data.table(read.table("extracted/UCI HAR Dataset/test/subject_test.txt", col.names=c("SUBJECT"), row.names=NULL, colClasses = c("numeric")))
 testActivity <- as.data.table(read.table("extracted/UCI HAR Dataset/test/y_test.txt", col.names=c("ACTIVITY_ID"), row.names=NULL, colClasses = c("numeric")))
-testData <- as.data.table(read.table("extracted/UCI HAR Dataset/test/x_test.txt", col.names=featureNames, row.names=NULL))
+testData <- as.data.table(read.table("extracted/UCI HAR Dataset/test/x_test.txt", col.names=featureNames, row.names=NULL, check.names = FALSE))
 # add subject to test data
-testData$SUBJECT <- testSubject[,1]
+testData$SUBJECT <- testSubject$SUBJECT
 # add activity to test data
 testActivitiesTable <- testActivity[,ACTIVITY := activityTable[ACTIVITY_ID,2]]
+testData$ACTIVITY <- testActivitiesTable$ACTIVITY
 
 # Merge test and train data
 data <- rbind(testData,trainData)
 
 # Filter columns by name
-columns <- which(grepl( "mean|std|ACTIVITY|SUBJECT", names(data)))
-filteredData <- data[, columns]
-
+columns <- which(grepl( "mean()|std()|ACTIVITY|SUBJECT", names(data)))
+filteredData <- as.data.frame(data)[, columns]
 dataTable <- as.data.table(filteredData)
-print(dataTable$SUBJECT[1:302])
-print(testActivity[1:302,])
-print(dataTable$ACTIVITY[1:302])
 tidyDataSet <- dataTable[, lapply(.SD,mean), by=c("SUBJECT", "ACTIVITY")]
 
 write.table(tidyDataSet, "tidydataset.txt", row.names=FALSE)
-
